47a38533aebaca5457bda469f798ee22e1ebd8d4
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-07-13 10:11:07 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoLocationDetection/MainActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c347.id19007966.demoLocationDetection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
